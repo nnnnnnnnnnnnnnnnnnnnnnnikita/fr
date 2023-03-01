82156ba454005cb11df2783db911d24701305f93
qwertyRaketa572 <nikitazuev141@yandex.ru>
2023-03-01 18:58:08 +0300

    om

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def index(us):
     user1 = "И на Марсе будут яблони цвести!"
     return render_template('index.html', title=us,
                            username=user, ert=user1)
+@app.route('/list_prof/<list_index>')
+def index1(list_index):
+    return render_template('list_prof.html', list_index=list_index)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
