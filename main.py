f70976eb75644ede04357bf0831a4c6612be1f3c
qwertyRaketa572 <nikitazuev141@yandex.ru>
2023-03-01 19:56:27 +0300

    om

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def index(us):
     user1 = "И на Марсе будут яблони цвести!"
     return render_template('index.html', title=us,
                            username=user, ert=user1)
+@app.route('/training/<prof>')
+def index2(prof):
+    if ("строитель" in prof) or ("инженер" in prof):
+        return render_template('card.html', prof="Инженерные тренажеры", image="f_staff.jpg")
+    else:
+        return render_template('card.html', prof="Научные симуляторы", image="s_st.jpg")
 @app.route('/list_prof/<list_index>')
 def index1(list_index):
     return render_template('list_prof.html', list_index=list_index)
