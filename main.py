70f07d2e5cac0217ba9e5b51c456d907dddf7367
qwertyRaketa572 <nikitazuev141@yandex.ru>
2023-03-01 21:15:22 +0300

    om

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ def index2(prof):
 def index1(list_index):
     return render_template('list_prof.html', list_index=list_index)
 
-@app.route('/distribution')
-def distribution():
-    astronauts = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
-    return render_template('cauti.html', astronauts=astronauts)
-
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
